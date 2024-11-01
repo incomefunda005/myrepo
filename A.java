db5dc343d63f84b706a652bfda8ea301b4e886b2
Shabir <incomefuna005@gmail.com>
2024-11-01 21:20:18 +0530

    make changes to sum code

--- A.java
+++ A.java
@@ -1,10 +1,10 @@
-import static java.lang.Integer.sum;
+
 
 public class A {
-    int a = 10;
-    int b = 20;
-    int c
     public static void main(String[] args) {
-        int c = sum(int a + int b);
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
     }
 }
